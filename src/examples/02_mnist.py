6627067fbe2922146d2b931de0a8b99866bcc267
alexander wickman <alexander.k.wickman@gmail.com>
2026-08-29 11:05:45 +0100

    0.3 Add manual optimiser
    Add step train functions for inline comparison

--- src/examples/02_mnist.py
+++ src/examples/02_mnist.py
@@ -1,3 +1,4 @@
+import copy
 
 import torch
 from torch import nn
@@ -57,6 +58,62 @@ def train_loop(dataloader, model: NeuralNetwork, loss_fn, optimiser: torch.optim
             current = batch * batch_size + len(X)
             print(f"loss: {loss.item():>7f}  [{current:>5d}/{size:>5d}]")
 
+def train_loop_manual(dataloader, model: NeuralNetwork, loss_fn):
+    size = len(dataloader.dataset)
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        # Prediction and loss
+        model.zero_grad()
+        pred = model(X)
+        loss = loss_fn(pred, y)
+        
+        # Backpropagation
+        loss.backward()
+        # Manual optimiser step
+        with torch.no_grad():
+            for p in model.parameters():
+                if p.grad is not None:
+                    # More accurate: p.add_(p.grad, alpha=-learning_rate)
+                    p -= learning_rate * p.grad
+        
+        if batch % 100 == 0:
+            current = batch * batch_size + len(X)
+            print(f"loss: {loss.item():>7f}  [{current:>5d}/{size:>5d}]")
+
+def step_train_optimiser(model: NeuralNetwork, loss_fn, optimiser: torch.optim.SGD, X, y, batch, size):
+    model.train()
+    optimiser.zero_grad()
+    pred = model(X)
+    loss = loss_fn(pred, y)
+    
+    # Backpropagation
+    loss.backward()
+    optimiser.step()
+    
+    if batch % 100 == 0:
+        current = batch * batch_size + len(X)
+        print(f"loss: {loss.item():>7f}  [{current:>5d}/{size:>5d}]")
+
+def step_train_manual(model: NeuralNetwork, loss_fn, X, y, batch, size):
+    model.train()
+    model.zero_grad()
+    pred = model(X)
+    loss = loss_fn(pred, y)
+    
+    # Backpropagation
+    loss.backward()
+    # Manual optimiser step
+    with torch.no_grad():
+        for p in model.parameters():
+            if p.grad is not None:
+                # More accurate: p.add_(p.grad, alpha=-learning_rate)
+                p -= learning_rate * p.grad
+    
+    if batch % 100 == 0:
+        current = batch * batch_size + len(X)
+        print(f"loss: {loss.item():>7f}  [{current:>5d}/{size:>5d}]")
+        
+
 def test_loop(dataloader, model: NeuralNetwork, loss_fn):
     model.eval()
     size = len(dataloader.dataset)
@@ -78,6 +135,7 @@ def main() -> None:
     device = "cpu"
     print(f"Using {device} device")
     model = NeuralNetwork().to(device)
+    model_opt = copy.deepcopy(model)
     print(model)
     
     
@@ -91,11 +149,27 @@ def main() -> None:
     loss_fn = nn.CrossEntropyLoss()
     
     optimiser = torch.optim.SGD(model.parameters(), lr=learning_rate)
+    size = len(train_dataloader.dataset) # type: ignore
+    # Test optimiser is equal
+    for batch, (X, y) in enumerate(train_dataloader):
+        step_train_optimiser(model, loss_fn, optimiser, X, y, batch, size)
+        step_train_manual(model_opt, loss_fn, X, y, batch, size)
+        if batch == 0:
+            # Show difference caused by first calculation due to difference in FP calc accuracy
+            print(f"maxdiff: {max((p-q).abs().max().item() for p,q in zip(model.parameters(), model_opt.parameters())):.3e}")
+    # train_loop(train_dataloader, model, loss_fn, optimiser)
+    # train_loop_manual(train_dataloader, model_opt, loss_fn)
     
-    for t in range(epochs):
-        print(f"Epoch {t+1}\n-------------------------------")
-        train_loop(train_dataloader, model, loss_fn, optimiser)
-        test_loop(test_dataloader, model, loss_fn)
+    identical = all(
+        torch.allclose(p, q)
+        for p, q in zip(model.parameters(), model_opt.parameters(), strict=True)
+    )
+    print(f"parameters identical: {identical}")
+    
+    # for t in range(epochs):
+    #     print(f"Epoch {t+1}\n-------------------------------")
+    #     train_loop(train_dataloader, model, loss_fn, optimiser)
+    #     test_loop(test_dataloader, model, loss_fn)
     
     # X = torch.rand(1, 28, 28, device=device)
     # print(X)
